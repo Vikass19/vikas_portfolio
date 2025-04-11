b60f0970cd054f8f50c05a4975fc3fecf7d21bec
Vikas Bansode <131449554+Vikass19@users.noreply.github.com>
2025-04-11 09:34:10 +0530

    Update Hero.jsx

--- src/componants/Hero.jsx
+++ src/componants/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import resume from "../assets/VIKAS BANSODE_resume_v2";
+import resume from "../assets/vikasResume_v2.pdf";
 import mypic from "../assets/profile.png";
 
 const Hero = () => {
